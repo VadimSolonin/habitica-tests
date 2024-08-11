4372452b003a28fec7a3c8d4b2320de7489bc155
VS <solonin.vadim@mail.ru>
2024-08-11 16:52:29 +0300

    hide steps from test pages

--- src/test/java/pages/ItemsPage.java
+++ src/test/java/pages/ItemsPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.visible;
@@ -14,30 +15,33 @@ public class ItemsPage {
             itemNameInputSearch = $(".form-control");
     private ElementsCollection ItemTitleCount = $$(".standard-page div h2");
 
+    @Step("Open home page")
     public ItemsPage openPage(String pageAddress) {
         open(pageAddress);
         return this;
     }
 
+    @Step("Click on the value 'Special' in the filter")
     public ItemsPage clickSpecialFilterCheckbox() {
         specialFilterCheckbox.click();
         return this;
     }
 
+    @Step("Check that the number of items on the page is one")
     public ItemsPage checkItemTitleCount(int titleCount) {
         ItemTitleCount.shouldHave(size(titleCount));
         return this;
     }
 
+    @Step("Make sure the page displays a special element")
     public ItemsPage checkSpecialItemVisibility(String titleText) {
         itemsTab.$(withText(titleText)).shouldBe(visible);
         return this;
     }
 
+    @Step("Enter the value `Item` in the search field")
     public ItemsPage setItemSearchName(String itemName) {
         itemNameInputSearch.setValue(itemName);
         return this;
     }
-
-
 }
--- src/test/java/tests/web/ItemsPageTests.java
+++ src/test/java/tests/web/ItemsPageTests.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import pages.ItemsPage;
 import tests.api.extensions.WithLogin;
 
-import static io.qameta.allure.Allure.step;
-
 @Feature("Items page testing")
 @Tag("web")
 public class ItemsPageTests extends TestBase {
@@ -21,18 +19,10 @@ public class ItemsPageTests extends TestBase {
     @DisplayName("Successful rendering of a special item when it is selected in the filter")
     @Story("Testing that when you select a special item in the filter, it is displayed on the page")
     public void verifyMapSpecialItemTest() {
-        step("Open home page", () -> {
-            itemsPage.openPage("inventory/items");
-        });
-        step("Click on the value 'Special' in the filter", () -> {
-            itemsPage.clickSpecialFilterCheckbox();
-        });
-        step("Check that the number of items on the page is one", () -> {
-            itemsPage.checkItemTitleCount(1);
-        });
-        step("Make sure the page displays a special element", () -> {
-            itemsPage.checkSpecialItemVisibility("Special");
-        });
+        itemsPage.openPage("inventory/items")
+                .clickSpecialFilterCheckbox()
+                .checkItemTitleCount(1)
+                .checkSpecialItemVisibility("Special");
     }
 
     @Test
@@ -40,14 +30,8 @@ public class ItemsPageTests extends TestBase {
     @DisplayName("Successfully rendering a special element when searching for it in the search bar")
     @Story("Testing that when you enter text from the description of a special element into the search bar, it is displayed on the page")
     public void searchItemInInventoryTest() {
-        step("Open home page", () -> {
-            itemsPage.openPage("inventory/items");
-        });
-        step("Enter the value `Item` in the search field", () -> {
-            itemsPage.setItemSearchName("Item");
-        });
-        step("Make sure the page displays a special element", () -> {
-            itemsPage.checkSpecialItemVisibility("Special");
-        });
+        itemsPage.openPage("inventory/items")
+                .setItemSearchName("Item")
+                .checkSpecialItemVisibility("Special");
     }
 }
